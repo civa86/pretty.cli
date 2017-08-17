30375f8ab03e04d2140f239e3ac3ed5e3e1e9c29
Dario Civallero <dario.civallero@etiqa.it>
2017-08-17 15:34:37 +0200

    ansi escapes

--- src/pretty/cli/ansi_escapes.clj
+++ src/pretty/cli/ansi_escapes.clj
@@ -4,33 +4,103 @@
 ;(def esc-code "debug-")
 
 (defn cursor-to
-  "Cursor to the 'x,y' position"
+  "Move the cursor to the 'x, y' cell"
   ([] (str esc-code "H"))
   ([x] (str esc-code (+ x 1) "G"))
   ([x y] (str esc-code (+ y 1) ";" (+ x 1) "H")))
 
 (defn cursor-move [x y]
-  "Move the cursor by 'x' and 'y' positions"
+  "Move the cursor by 'x' and 'y' cells"
   (let [x-code (if (< x 0) (str (- x) "D") (str x "C"))
         y-code (if (< y 0) (str (- y) "A") (str y "B"))]
     (str esc-code x-code esc-code y-code)))
 
 (defn cursor-up
-  "Move the cursor up by 'n' positions"
+  "Moves the cursor 'n' (default 1) cells up"
   ([n] (str esc-code n "A"))
   ([] (cursor-up 1)))
 
 (defn cursor-down
-  "Move the cursor down by 'n' positions"
+  "Moves the cursor 'n' (default 1) cells down"
   ([n] (str esc-code n "B"))
   ([] (cursor-down 1)))
 
 (defn cursor-forward
-  "Move the cursor forward by 'n' positions"
+  "Moves the cursor 'n' (default 1) cells forward"
   ([n] (str esc-code n "C"))
   ([] (cursor-forward 1)))
 
 (defn cursor-backward
-  "Move the cursor backward by 'n' positions"
+  "Moves the cursor 'n' (default 1) cells backward"
   ([n] (str esc-code n "D"))
   ([] (cursor-backward 1)))
+
+(defn cursor-left [] "Moves the cursor to the left" (str esc-code "G"))
+
+(defn cursor-position []
+  "Retrieve the cursor position"
+  (str esc-code "6n"))
+
+(defn cursor-position-save []
+  "Save the cursor position"
+  (str esc-code "s"))
+
+(defn cursor-position-restore []
+  "Restore the cursor position"
+  (str esc-code "u"))
+
+(defn cursor-next-line []
+  "Moves the cursor to beginning of the next line"
+  (str esc-code "E"))
+
+(defn cursor-prev-line []
+  "Moves the cursor to beginning of the previous line"
+  (str esc-code "F"))
+
+(defn cursor-hide []
+  "Hide the cursor"
+  (str esc-code "?25l"))
+
+(defn cursor-show []
+  "Show the cursor"
+  (str esc-code "?25h"))
+
+(defn erase-end-line []
+  "Erase to the end of the line"
+  (str esc-code "K"))
+
+(defn erase-start-line []
+  "Erase to the start of the line"
+  (str esc-code "1K"))
+
+(defn erase-line []
+  "Erase the line"
+  (str esc-code "2K"))
+
+(defn erase-down []
+  "Erase down"
+  (str esc-code "J"))
+
+(defn erase-up []
+  "Erase up"
+  (str esc-code "1J"))
+
+(defn erase-screen []
+  "Erase the screen"
+  (str esc-code "2J"))
+
+(defn scroll-up []
+  "Scroll up"
+  (str esc-code "S"))
+
+(defn scroll-down []
+  "Scroll down"
+  (str esc-code "T"))
+
+(defn clear-screen []
+  "Clear screen"
+  (str "\u001Bc"))
+
+(defn beep []
+  "beep"
+  (str "\u0007"))
--- test/pretty/cli/ansi_escapes_test.clj
+++ test/pretty/cli/ansi_escapes_test.clj
@@ -30,4 +30,57 @@
   (testing "cursor-backward"
     (is (= (cursor-backward) "\033[1D"))
     (is (= (cursor-backward 5) "\033[5D")))
-  )
+
+  (testing "cursor-left"
+    (is (= (cursor-left) "\033[G")))
+
+  (testing "cursor-position"
+    (is (= (cursor-position) "\033[6n")))
+
+  (testing "cursor-position-save"
+    (is (= (cursor-position-save) "\033[s")))
+
+  (testing "cursor-position-restore"
+    (is (= (cursor-position-restore) "\033[u")))
+
+  (testing "cursor-next-line"
+    (is (= (cursor-next-line) "\033[E")))
+
+  (testing "cursor-prev-line"
+    (is (= (cursor-prev-line) "\033[F")))
+
+  (testing "cursor-hide"
+    (is (= (cursor-hide) "\033[?25l")))
+
+  (testing "cursor-show"
+    (is (= (cursor-show) "\033[?25h")))
+
+  (testing "erase-end-line"
+    (is (= (erase-end-line) "\033[K")))
+
+  (testing "erase-start-line"
+    (is (= (erase-start-line) "\033[1K")))
+
+  (testing "erase-line"
+    (is (= (erase-line) "\033[2K")))
+
+  (testing "erase-down"
+    (is (= (erase-down) "\033[J")))
+
+  (testing "erase-up"
+    (is (= (erase-up) "\033[1J")))
+
+  (testing "erase-screen"
+    (is (= (erase-screen) "\033[2J")))
+
+  (testing "scroll-up"
+    (is (= (scroll-up) "\033[S")))
+
+  (testing "scroll-down"
+    (is (= (scroll-down) "\033[T")))
+
+  (testing "clear-screen"
+    (is (= (clear-screen) "\u001Bc")))
+
+  (testing "beep"
+    (is (= (beep) "\u0007"))))
