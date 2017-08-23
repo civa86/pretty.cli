4a6a3386aab83d36e8e20256283d4129b831f7bd
Dario Civallero <dario.civallero@etiqa.it>
2017-08-23 17:26:36 +0200

    deploy clojars

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject org.clojars.civa86/pretty.cli "0.2.1"
+(defproject org.clojars.civa86/pretty.cli "0.3.0"
   :description "Clojure utilities to create amazing Command Line Interfaces"
   :url "https://github.com/civa86/pretty.cli"
   :license {:name "MIT"
