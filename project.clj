d8e6b4bb085085be4c0fa719e1a644ec97688660
Dario Civallero <dario.civallero@etiqa.it>
2017-09-04 11:13:55 +0200

    disabled sign release

--- project.clj
+++ project.clj
@@ -2,13 +2,14 @@
   :description "Clojure utilities to create amazing Command Line Interfaces"
   :url "https://github.com/civa86/pretty.cli"
   :license {:name "MIT"
-            :url "https://opensource.org/licenses/MIT"}
+            :url  "https://opensource.org/licenses/MIT"}
   :dependencies [[org.clojure/clojure "1.8.0"]
                  [prismatic/schema "1.1.6"]
                  [com.github.lalyos/jfiglet "0.0.8"]
                  [jline "0.9.94"]]
-  :deploy-repositories [["clojars" {:url "https://clojars.org/repo/"
-                                     :username :env/clojars_user
-                                     :password :env/clojars_passwd}]]
+  :deploy-repositories [["clojars" {:url           "https://clojars.org/repo/"
+                                    :username      :env/clojars_user
+                                    :password      :env/clojars_passwd
+                                    :sign-releases false}]]
   :profiles {:dev {:plugins [[com.jakemccrary/lein-test-refresh "0.18.1"]
                              [lein-codox "0.10.3"]]}})
