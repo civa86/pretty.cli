ef76f8a9dacf43147f45aff9bc0036b08ca3acd1
Dario Civallero <dario.civallero@etiqa.it>
2017-09-04 17:43:59 +0200

    codox enhancement

--- project.clj
+++ project.clj
@@ -6,10 +6,16 @@
   :dependencies [[org.clojure/clojure "1.8.0"]
                  [prismatic/schema "1.1.6"]
                  [com.github.lalyos/jfiglet "0.0.8"]
-                 [jline "0.9.94"]]
+                 [jline "0.9.94"]
+                 [codox-theme-rdash "0.1.2"]]
+  :codox {:metadata {:doc/format :markdown}
+          :themes [:rdash]
+          :html {:namespace-list :flat}
+          :source-uri "https://github.com/civa86/pretty.cli/blob/v{version}/{filepath}#L{line}"}
   :deploy-repositories [["clojars" {:url           "https://clojars.org/repo/"
                                     :username      :env/clojars_user
                                     :password      :env/clojars_passwd
                                     :sign-releases false}]]
   :profiles {:dev {:plugins [[com.jakemccrary/lein-test-refresh "0.18.1"]
                              [lein-codox "0.10.3"]]}})
+
