d73176ae6588383516eefdb2e70ac8d586d05666
Dario Civallero <dario.civallero@etiqa.it>
2017-09-06 17:13:32 +0200

    bump version

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject org.clojars.civa86/pretty.cli "0.6.1"
+(defproject org.clojars.civa86/pretty.cli "0.6.2"
   :description "Clojure utilities to create amazing Command Line Interfaces"
   :url "https://github.com/civa86/pretty.cli"
   :license {:name "MIT"
